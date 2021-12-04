429a78e347af9daca1c267fc3a6a3c8d4f2c51fe
Vanakoff <andrey.vanakoff@gmail.com>
2021-12-05 00:43:19 +0100

    Main mockup in storyboard created

--- MusicGenerator/ViewController.swift
+++ MusicGenerator/ViewController.swift
@@ -17,7 +17,24 @@ class ViewController: UIViewController {
        
         logoImageView.layer.cornerRadius = logoImageView.frame.width / 2
     }
-
-
+    
+    
+    @IBAction func howItWorksButtonTapped() {
+        showAlert(title: "Music Generator", message: "Choose answers that matches best on current you to get the most interesting musical playlist")
+    }
+    
+    @IBAction func aboutAppButtonTapped() {
+        showAlert(title: "Information", message: "Pet project, MVC based app")
+    }
+    
 }
 
+//MARK: Extensions
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
